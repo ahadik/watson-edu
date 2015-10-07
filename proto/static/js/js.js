1a79f182e485151a44a7e08c2595bd501d9c0400
ahadik <ahadik@us.ibm.com>
2015-10-07 15:00:28 -0500

    Added messaging capabilities

--- proto/static/js/js.js
+++ proto/static/js/js.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	
 	var prev_dragger_pos = $('#dragger').position().left;
+	var area;
+	var span;
 	
 	$('#dragger').draggable({
 		axis: 'x',
@@ -20,12 +22,17 @@ $(document).ready(function(){
 		}	
 	});
 	
+	function updateMessageHeight(){
+		span.textContent = area.value;
+		$('#chat_transcript').css({'height': $('#chat').height()-$('#chat_box').height()-47});
+	}
+	
 	function makeExpandingArea(container) {
-		var area = container.querySelector('textarea');
-		var span = container.querySelector('span');
+		area = container.querySelector('textarea');
+		span = container.querySelector('span');
 		if (area.addEventListener) {
 			area.addEventListener('input', function() {
-				span.textContent = area.value;
+				updateMessageHeight();
 			}, false);
 			span.textContent = area.value;
 		} else if (area.attachEvent) {
@@ -37,7 +44,53 @@ $(document).ready(function(){
 		}
 		// Enable extra CSS
 		container.className += " active";
-		}var areas = document.querySelectorAll('.expandingArea');
+	}
+	
+	function appendMessage(message, user_code){
+		/*
+			USER_CODE: 0 => WATSON
+			USER_CODE: 1 => USER
+		*/
+		
+		var message_el = $(
+			'<div class="message_wrapper" style="display: none;">'+
+				'<div class="message">'+
+					'<div class="indicator_bar"></div>'+
+					'<div class="message_body">'+
+						'<p>'+message+'</p>'+
+					'</div>'+
+				'</div>'+
+			'</div>'
+		);
+		
+		if (user_code === 0){
+			$(message_el).addClass('watson');
+		}else if(user_code === 1){
+			$(message_el).addClass('user');
+		}
+		
+		$('h1:visible,h2:visible', $('#chat_transcript')).fadeOut(400, function(){
+			var transcript = $('#chat_transcript');
+			transcript.append(message_el).animate({scrollTop : transcript.height()}, 400);
+			message_el.fadeIn();
+		});
+	}
+	
+	$('textarea[name="chat_input"]').on('keyup', function(e){
+		e = e || event;
+		if (e.keyCode === 13){
+			var message_text = $('textarea[name="chat_input"]').val();
+			appendMessage(message_text, 1);
+			$('textarea[name="chat_input"]').val('');
+			updateMessageHeight();
+		}
+		return true;
+	});
+	
+	$('#chat_transcript').css({'height': $('#chat').height()-$('#chat_box').height()-15});
+	
+	
+		var areas = document.querySelectorAll('.expandingArea');
 		var l = areas.length;while (l--) {
 			makeExpandingArea(areas[l]);
 		}
